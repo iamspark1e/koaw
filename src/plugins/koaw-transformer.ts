e7f22a24575bcaf77a302d2e393fdc854e92f421
arctome <hi@arcto.me>
2021-10-19 07:14:32 +0000

    fix: `Transformer.responseToCtx` throw error when response does not contain "Content-Type" header

--- src/plugins/koaw-transformer.ts
+++ src/plugins/koaw-transformer.ts
@@ -12,21 +12,24 @@ async function responseToCtx(response: Response) {
   res.statusText = response.statusText;
   // @ts-ignore
   res.headers = proxyGetHeader(response.headers);
-  if (res.headers["content-type"].includes("application/json")) {
-    res.body = await response.json();
-  } else if (
-    res.headers["content-type"].includes("application/x-www-form-urlencoded")
-  ) {
-    let urlSearch = await response.text();
-    // @ts-ignore
-    res.body = Object.fromEntries(new URLSearchParams(urlSearch).entries());
-  } else if (res.headers["content-type"].includes("text/")) {
-    // Just match all types with 'text/' prefix
-    res.body = await response.text();
+  if (res.headers["content-type"]) {
+    if (res.headers["content-type"].includes("application/json")) {
+      res.body = await response.json();
+    } else if (
+      res.headers["content-type"].includes("application/x-www-form-urlencoded")
+    ) {
+      let urlSearch = await response.text();
+      // @ts-ignore
+      res.body = Object.fromEntries(new URLSearchParams(urlSearch).entries());
+    } else if (res.headers["content-type"].includes("text/")) {
+      // Just match all types with 'text/' prefix
+      res.body = await response.text();
+    } else {
+      res.body = await response.blob();
+    }
   } else {
     res.body = await response.blob();
   }
-
   return res;
 }
 
